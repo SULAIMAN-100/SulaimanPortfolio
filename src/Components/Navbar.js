a3a0ab619606b2953074e1b9a792d8fe5b826b18
Sulaiman <sulaimansolo73505@gmail.com>
2021-06-20 00:23:27 +0100

    fixed email

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -69,7 +69,7 @@ function Navbar() {
                       <a
                         rel="#"
                         target="_blank"
-                        href="mailto:sulaimansolo73505@gmail.com.com"
+                        href="mailto:sulaimansolo73505@gmail.com"
                       >
                         sulaimansolo73505@gmail.com
                       </a>
