de0fe781e7f161c02f541e4dbd07d4fa8f91cbf5
Sulaiman <sulaimansolo73505@gmail.com>
2021-06-19 21:30:14 +0100

    changed footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,9 +10,21 @@ function Contact() {
       <div className="footer">
         <p>SULAIMAN@2021</p>
         <div className="social">
-          <img src={In} />
-          <img src={Github} />
-          <img src={Email} />
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/sulaiman-alhalabi-4038521b0/"
+          >
+            <img src={In} />
+          </a>
+          <a target="_blank" href="https://github.com/SULAIMAN-100">
+            <img src={Github} />
+          </a>
+          <a
+            target="_blank"
+            href="mailto:suliamnsolo73505@gmail.com?subject = Feedback&body = Message"
+          >
+            <img src={Email} />
+          </a>
         </div>
       </div>
     </div>
